a9ddc5b87d4def7e65c988a7a17a6af37394fc9b
abrham123 <abr_mere@yahoo.com>
2016-10-12 14:07:27 -0400

    hi I commited again

---
+++ Xsuper/src/Xclient.java
@@ -0,0 +1,17 @@
+/**
+ * @author Java
+ */
+public class Xclient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Xchild xx = new Xchild(45, "Vinod");
+		System.out.println(xx.getAge() + "   " + xx.getName());
+
+	}
+
+}
---
+++ Xsuper/src/Xsuper.java
@@ -0,0 +1,20 @@
+public class Xsuper {
+
+	private String	name;
+
+	Xsuper() {
+	}
+
+	Xsuper(String xxx) {
+		this.name = xxx;
+
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
