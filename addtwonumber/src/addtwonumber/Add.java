9cab82d6add48b82d8604eb8e040f86beaa4becd
abrham123 <abr_mere@yahoo.com>
2016-10-12 14:22:19 -0400

    berhanu do push and commit to add classs

---
+++ addtwonumber/src/addtwonumber/Add.java
@@ -0,0 +1,14 @@
+package addtwonumber;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 6;
+		int y = 7;
+		int sum;
+		sum = x + y;
+		System.out.println("The sum of " + x + "and " + y + "is " + sum);
+
+	}
+}
